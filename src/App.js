1729fc060fe746d072b142226056a14cb055282d
12priyanka-singh <priyankagtm31@gmail.com>
2017-07-07 17:41:25 +0530

    Added google sign in button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-import GoogleLogin from 'react-google-login';
-// import { Route, Redirect } from 'react-router'; 
+import LoginButton from "./LoginButton";
+
+// var Template = require('./Template');
+// window.React = React;
+
+/* global gapi */
+/* global LoginButton */
 
 class App extends Component {
 
@@ -14,22 +17,93 @@ class App extends Component {
       loggedIn: false,
       todoMap: {}
     };
+
+
+
+  //       script.src = "https://apis.google.com/js/platform.js";
+  //       script.async = true;
+  //       script.defer = true;
+
+  //       document.body.appendChild(script);
+
+
+  //   gapi.signin2.render('g-signin2', {
+  //   'scope': 'https://www.googleapis.com/auth/plus.login',
+  //   'width': 200,
+  //   'height': 50,
+  //   'longtitle': true,
+  //   'theme': 'dark',
+  //   'onsuccess': this. onSignIn
+  // });  
   }
 
-  errorResponseGoogle = (response) => {
-      console.log(response);
+  loadGoogleApi(){
+
+    // const script = document.createElement("script");
+    // script.src = "https://apis.google.com/js/platform.js";
+    // script.async = true;
+    // script.defer = true;
+
+    // script.onload = () => {
+    //   gapi.load('client', () => {
+    //     gapi.client.setApiKey(API_KEY);
+    //     gapi.client.load('youtube', 'v3', () => {
+    //       this.setState({ gapiReady: true });
+    //     });
+    //   });
+    // };
+
+    // document.body.appendChild(script);
   }
 
-  successResponseGoogle = (response) => {
-      this.setState(
-          { 
-              loggedIn : true,
-              user : response.profileObj,
-              email : response.profileObj.email
-          }
-        );
-      console.log(response);
+  componentDidMount() {
+    this.loadGoogleApi();
   }
+
+  // onSignIn = (googleUser) => {
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
+
+//   componentDidMount = () => {
+//   gapi.signin2.render('g-signin2', {
+//     'scope': 'https://www.googleapis.com/auth/plus.login',
+//     'width': 200,
+//     'height': 50,
+//     'longtitle': true,
+//     'theme': 'dark',
+//     'onsuccess': this. onSignIn
+//   });  
+// }
+
+  // errorResponseGoogle = (response) => {
+  //     console.log(response);
+  // }
+
+  // successResponseGoogle = (response) => {
+  //     this.setState(
+  //         { 
+  //             loggedIn : true,
+  //             user : response.profileObj,
+  //             email : response.profileObj.email
+  //         }
+  //       );
+  //     console.log(response);
+  // }
+
+
+  // componentWillMount() {
+  //       const script = document.createElement("script");
+
+  //       script.src = "https://apis.google.com/js/platform.js";
+  //       script.async = true;
+  //       script.defer = true;
+
+  //       document.body.appendChild(script);
+  //   }
 
   logout = () => {
       this.setState(
@@ -106,45 +180,44 @@ class App extends Component {
         
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React Part 2</h2>
+        
         </div>
         
-    
-        {
-          this.state.loggedIn ? 
-            <div>
-              <h3> Welcome {this.state.user.name}        
-                <button type="submit" onClick={this.logout} style={{textAlign:"right"}} color="#2E8B57" >Log out</button>
-              </h3> 
 
-            
-                <input type="text" name="todoText" id="todoText" placeholder="Title..." size="100" />
-              
-
-              <button type="submit" onClick={this.addTodo}>Add Todo</button> 
-              {
-                this.state.todoMap[this.state.email] && <ol type="1" > {this.getArray()} </ol>
-              }
-            </div>
-            :
-            <div>
-              <h1></h1>
-              <GoogleLogin 
-                  clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" 
-                  buttonText="Login" 
-                  onSuccess={this.successResponseGoogle}
-                  onFailure={this.errorResponseGoogle} >
-
-                  <span> Login with Google</span>
-              </GoogleLogin>
-            </div>
-          }
-          
       </div>
     );
-
-
   }
+    
+       // {
+       //    this.state.loggedIn ? 
+       //      <div>
+       //        <h3> Welcome {this.state.user.name}        
+       //          <button type="submit" onClick={this.logout} style={{textAlign:"right"}} color="#2E8B57" >Log out</button>
+       //        </h3> 
+
+            
+       //          <input type="text" name="todoText" id="todoText" placeholder="Title..." size="100" />
+              
+
+       //        <button type="submit" onClick={this.addTodo}>Add Todo</button> 
+       //        {
+       //          this.state.todoMap[this.state.email] && <ol type="1" > {this.getArray()} </ol>
+       //        }
+       //      </div>
+       //      :
+       //      <div>
+       //        <h1></h1>
+       //        <GoogleLogin 
+       //            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com" 
+       //            buttonText="Login" 
+       //            onSuccess={this.successResponseGoogle}
+       //            onFailure={this.errorResponseGoogle} >
+
+       //            <span> Login with Google</span>
+       //        </GoogleLogin>
+       //      </div>
+       //    }
 }
 export default App;
 
